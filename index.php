29a6ee57f17f429cbdae965ed59d290a07cad4c7
David Jegat <david.jegat@gmail.com>
2012-05-01 17:41:04 +0200

    File for a simple test render
    simple test

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+function test () {
+    echo 'test';
+}
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Test</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h1>Hello world !</h1>
+        <p>This is the test function : <?php test(); ?></p>
+    </body>
+</html>
